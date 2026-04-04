e76fd0bcaff0da8b274e3be2bf41a5f32ab8fbe0
Logan Burns <lburns1@ufl.edu>
2026-04-04 18:09:05 -0400

    fixed clock files

---
+++ synth/clock.h
@@ -0,0 +1,18 @@
+/*
+;************************************************************************
+;  Name: Logan Burns
+;  Description: Modify the system clock frequency
+;************************************************************************
+*/
+
+#ifndef CLOCK_H_
+#define CLOCK_H_
+
+
+#include <avr/io.h>
+#include <avr/cpufunc.h>
+#include "common.h"
+
+void init_clk(void);
+	
+#endif /* COMMON_H_ */
